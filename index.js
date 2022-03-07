61008425511861f4fd01fbe8a803ff5b8271de76
hailing_xu <66230255+u654@users.noreply.github.com>
2022-03-07 10:03:01 +0800

    random game

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+alert("working");
+var randomNumber1 = Math.floor(Math.random() * 6+1);
+var root="images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",root);
+var randomNumber2 = Math.floor(Math.random() * 6+1);
+var root="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",root);
+if (randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩play 1 wins!";
+}
+else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").innerHTML="play 2 wins!🚩";
+}
+else if (randomNumber1=randomNumber2) {
+  document.querySelector("h1").innerHTML="Draw!";
+}
